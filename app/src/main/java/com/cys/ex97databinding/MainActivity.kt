b6489cdcd2865c0f17c926d718a25c7e5bbfe62e
dragonechoi <vasco1379@naver.com>
2023-05-25 15:31:13 +0900

    바인딩 연결 과정

--- app/src/main/java/com/cys/ex97databinding/MainActivity.kt
+++ app/src/main/java/com/cys/ex97databinding/MainActivity.kt
@@ -2,10 +2,18 @@ package com.cys.ex97databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.cys.ex97databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //데이터 바인딩에서는 뷰바인딩과 다르게 레이아웃 xml 파일의
+    // root 요소가 <layout> 이어야만 binding 클래스가 만들어짐
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
     }
 }
